5122d45125d4c7951ae061a96105b28ecf8cd8c8
Addis Eshetu <addiseshetu@Addiss-MacBook-Pro.local>
2021-11-03 15:41:15 -0400

    First commit

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Ethiopian vegan of the day</h1>
+      <p>Misir wet begomen enditafit hono yetesera</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
---
+++ src/components/Products/Data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/ethiopian_vegan1.jpeg';
+import product2 from '../../images/ethiopian_vegan1.jpeg';
+import product3 from '../../images/ethiopian_vegan1.jpeg';
+import sweet1 from '../../images/sweet-1.jpeg';
+import sweet2 from '../../images/sweet-2.jpeg';
+import sweet3 from '../../images/sweet-3.jpeg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Miser',
+    name: 'Yetsom beyayinetu',
+    desc:
+      'Berbere, kibe, tikur azmud,tomatoes, shiro, and kariya',
+    price: '$19.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Gomen',
+    name: 'To die for',
+    desc:
+      ' Be olive oil tekulalto yetezegaje gomen',
+    price: '$16.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Alicha',
+    name: 'Veggie Overload',
+    desc:
+      ' Yeun zerzire yetun litewew',
+    price: '$14.99',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Kita',
+    name: 'Kita',
+    desc:
+      'Be olive oil ena be berbere yetashe kita',
+    price: '$9.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Chiko',
+    name: 'Chiko nefse',
+    desc:
+      'Gebse, kibena berbere sikelakelu yemifetrut teamir',
+    price: '$12.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Genfo',
+    name: 'Genfo',
+    desc:
+      'Sigezu yautal',
+    price: '$9.99',
+    button: 'Add to Cart'
+  }
+];
